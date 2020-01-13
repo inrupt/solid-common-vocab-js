f2bac9a23d79f204d68e642dcec250b8b8422151
Nicolas Seydoux <nicolas.seydoux@aliceadsl.fr>
2020-01-13 16:00:41 +0100

    Added a release script to prevent from republication

---
+++ .github/workflows/release-if-updated.sh
@@ -0,0 +1,13 @@
+# This script expects the path to the module directory as an argument
+cd $1
+# Get the package name from the package.json file
+packageName=`node -p "require('./package.json').name"`
+publishedVersion=`npm view $packageName --registry https://npm.pkg.github.com version `
+newVersion=`node -p "require('./package.json').version"`
+# Only proceed if the version about to be published has not been published previously
+if [ $newVersion != $publishedVersion ]
+then
+    npm install --registry https://npm.pkg.github.com/pmcb55
+    npm test
+    npm publish --registry https://npm.pkg.github.com/pmcb55
+fi
