1bd20c63e9e65cdc8938ea6eed7d67c983081f83
Pat McBennett <pat.mcbennett@gmail.com>
2020-02-04 09:33:35 +0000

    Update release-if-updated.sh

--- .github/workflows/release-if-updated.sh
+++ .github/workflows/release-if-updated.sh
@@ -1,10 +1,10 @@
-# This script expects the path to the module directory as an argument
+# This script expects the path to the module directory as an argument.
 cd $1
-# Get the package name from the package.json file
+# Get the package name from the package.json file.
 packageName=`node -p "require('./package.json').name"`
 publishedVersion=`npm view $packageName --registry https://npm.pkg.github.com version `
 newVersion=`node -p "require('./package.json').version"`
-# Only proceed if the version about to be published has not been published previously
+# Only proceed if the version about to be published has not been published previously.
 if [ $newVersion != $publishedVersion ]
 then
     npm install --registry https://npm.pkg.github.com/pmcb55
