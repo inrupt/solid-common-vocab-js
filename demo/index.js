e307ec4a0e29f2ce147e8fbff9e9938d0421eef7
pmcb55 <patm@inrupt.com>
2020-02-05 10:09:50 +0000

    Minor tidy up to the demo code and config.

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,11 @@
-const {LitVocabTermRdfExt} = require('@pmcb55/lit-vocab-term-rdf-ext')
+const {LitVocabTermRdfExt:LitVocabTerm} = require('@pmcb55/lit-vocab-term-rdf-ext')
 require('mock-local-storage')
 
-const person = new LitVocabTermRdfExt('https://example.com#Person', localStorage, true)
+const person = new LitVocabTerm('https://example.com#Person', localStorage, true)
   .addLabel('Person','en')
   .addLabel('Personne','fr')
   .addLabel('人', 'ch')
   .addComment('A human being, alive, dead or imaginary.','en')
 
 console.log(`The default label for the class ${person.value} is ${person.label.value}`)
-console.log(`The chinese label for the class ${person.value} is ${person.asLanguage('ch').label.value}`)
+console.log(`The Chinese label for the class ${person.value} is ${person.asLanguage('ch').label.value}`)
