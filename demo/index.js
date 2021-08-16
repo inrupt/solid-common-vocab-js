f095efa5dcbd2dd8bbde8123dc7c2502dcaad6a6
pmcb55 <patm@inrupt.com>
2021-08-16 14:42:39 +0200

    Updated to official RDF/JS types

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,48 @@
-const { VocabTerm } = require('@inrupt/solid-common-vocab')
-require('mock-local-storage')
+// Normally we would import the package from a repository, but for this demo
+// we can access the code directly.
+const { VocabTerm } = require("../");
+// In your code, you would normally do:
+// const { VocabTerm } = require('@inrupt/solid-common-vocab')
 
-const person = new VocabTerm('https://example.com#Person', localStorage, true)
-  .addLabel('Person','en')
-  .addLabel('Personne','fr')
-  .addLabel('人', 'ch')
-  .addComment('A human being, alive, dead or imaginary.','en')
+// We need to provide an implementation for the RDF/JS interfaces. There are a
+// number of implementations you can choose from, we've chosen this one:
+const { DataFactory } = require("rdf-data-factory");
 
-console.log(`The default label for the class ${person.value} is ${person.label.value}`)
-console.log(`The Chinese label for the class ${person.value} is ${person.asLanguage('ch').label.value}`)
+// Since we run as a simple Node.JS application, we need to provide some
+// implementation of 'LocalStorage' that always exists in browsers.
+require("mock-local-storage");
+
+// Now we can create a single vocabulary term that has a bunch of meta-data
+// associated with it.
+const person = new VocabTerm(
+  "https://example.com#Person",
+  new DataFactory(),
+  localStorage,
+  true
+)
+  .addLabel("Person", "en")
+  .addLabel("Personne", "fr")
+  .addLabel("人", "ch")
+  .addComment("A human being, alive, dead or imaginary.", "en");
+
+// Now we can access and display the meta-data associated with our new
+// vocabulary term.
+console.log(
+  `The default label for the class [${person.value}] is [${person.label}]`
+);
+
+console.log(
+  `The French label for the class [${person.value}] is [${
+    person.asLanguage("fr").label
+  }]`
+);
+
+console.log(
+  `The Chinese label for the class [${person.value}] is [${
+    person.asLanguage("ch").label
+  }]`
+);
+
+console.log(
+  `The default comment for the class [${person.value}] is [${person.comment}]`
+);
